b190082638d3c932d59f45a170ab229f63a93969
admin <admin@U1PC13>
2022-11-04 10:42:56 +0100

    Přidejte soubory projektu.

---
+++ cykly_s_mocninami/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Vitejte v našem mocninátorovi");
+Console.WriteLine("Zadejte základ mocnin");
+string z_klavesnice = Console.ReadLine();
+double zaklad = Convert.ToDouble(z_klavesnice);
+Console.WriteLine("Zadejte exponent");
+string z_klavesnice2 = Console.ReadLine();
+int exponent = Convert.ToInt16(z_klavesnice2);
+double umocneni;
+umocneni = zaklad;
+for (int i = 0; i<(exponent - 1); i++)
+    umocneni = umocneni * zaklad;
+Console.WriteLine("Výsledek: {0}", umocneni);
+Console.WriteLine("Děkujeme za pužití našeho programu");
+Console.ReadKey();
+
